bcb581789ca8034b224dcf3b23d0f2ea7e239e63
Adrien DE SEDE <adrien.de.sede@gmail.com>
2024-07-05 00:20:39 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,11 @@
 #[allow(unused_imports)]
 use std::env;
 use std::{
-    fs::{DirBuilder, File},
+    fs::File,
     io::{BufRead, BufReader, Read, Write},
 };
 
+use anyhow::{anyhow, Context};
 use sha1::{Digest, Sha1};
 
 use clap::{Parser, Subcommand};
@@ -36,116 +37,130 @@ enum Command {
 #[allow(unused_imports)]
 use std::fs;
 
-fn main() -> std::io::Result<()> {
+#[derive(Eq, PartialEq)]
+enum ObjectType {
+    Blob,
+}
+
+struct GitObject {
+    object_type: ObjectType,
+    _size: usize,
+    data: Vec<u8>,
+}
+
+// `<blob> <size>\0<content>`.
+fn parse_git_object(mut reader: BufReader<ZlibDecoder<File>>) -> Result<GitObject, anyhow::Error> {
+    let mut buf = Vec::new();
+
+    reader.read_until(b' ', &mut buf)?;
+
+    let object_type = if buf.starts_with(b"blob") {
+        ObjectType::Blob
+    } else {
+        let object_type = std::str::from_utf8(&buf).context("not utf8 ?")?;
+        let object_type = &object_type[0..object_type.len() - 1];
+
+        return Err(anyhow!("Object type `{object_type}` is not supported"));
+    };
+
+    buf.clear();
+    reader.read_until(0, &mut buf)?;
+
+    let size = std::str::from_utf8(&buf)
+        .context("not utf8 ?")?
+        .parse::<usize>()
+        .context("not a number ?")?;
+
+    buf.clear();
+    let n = reader.read_to_end(&mut buf)?;
+
+    if size != n {
+        anyhow::bail!("Expected {size} bytes, got {n}");
+    }
+
+    return Ok(GitObject {
+        object_type,
+        _size: size,
+        data: buf,
+    });
+}
+
+fn main() -> Result<(), anyhow::Error> {
     let args = Args::parse();
 
     match args.command {
         Command::Init => {
-            fs::create_dir(".git").unwrap();
-            fs::create_dir(".git/objects").unwrap();
-            fs::create_dir(".git/refs").unwrap();
-            fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+            fs::create_dir(".git")?;
+            fs::create_dir(".git/objects")?;
+            fs::create_dir(".git/refs")?;
+            fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
             println!("Initialized git directory")
         }
         Command::CatFile {
             pretty_print,
             object_hash,
         } => {
-            if !pretty_print {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::InvalidInput,
-                    "git cat-file: -p option is mandatory",
-                ));
-            }
+            anyhow::ensure!(pretty_print, "expected pretty print (-p)");
 
             let dirname = &object_hash[0..2];
             let filename = &object_hash[2..];
             let path = std::format!(".git/objects/{dirname}/{filename}");
 
-            if let Ok(file) = File::open(&path) {
-                let mut buf = Vec::new();
+            match File::open(&path) {
+                Ok(file) => {
+                    let decoder = ZlibDecoder::new(file);
+                    let reader = BufReader::new(decoder);
+                    let object = parse_git_object(reader)?;
 
-                let decoder = ZlibDecoder::new(file);
-                let mut decoder = BufReader::new(decoder);
+                    anyhow::ensure!(
+                        object.object_type == ObjectType::Blob,
+                        "cat-file can only read blob objects"
+                    );
 
-                // `<blob|commit|tag|tree> <size>\0<content>`
-                // Note: delimiter is captured
-                decoder.read_until(0, &mut buf).unwrap();
-
-                let metadata = &buf[0..buf.len() - 1];
-                let mut iter = metadata.split(|&x| x == b' ');
-
-                let object_type = iter.next().unwrap();
-                let object_type = std::str::from_utf8(object_type).unwrap();
-
-                let object_size = iter.next().unwrap();
-                let object_size = std::str::from_utf8(object_size).unwrap();
-                let _object_size = object_size.parse::<usize>().unwrap();
-
-                if object_type != "blob" {
-                    return Err(std::io::Error::new(
-                        std::io::ErrorKind::InvalidData,
-                        std::format!("Unsupported object type {}", object_type),
-                    ));
+                    print!("{}", std::str::from_utf8(&object.data)?);
                 }
-
-                buf.clear();
-                decoder.read_to_end(&mut buf).unwrap();
-
-                let content = String::from_utf8(buf).unwrap();
-                print!("{}", content);
-            } else {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::NotFound,
-                    std::format!("No such file or directory: {}", &path),
-                ));
+                Err(_) => {
+                    anyhow::bail!("No such file or directory: {}", &path);
+                }
             }
         }
-        Command::HashObject { write, filename } => {
-            if let Ok(input_file) = File::open(&filename) {
+        Command::HashObject { write, filename } => match File::open(&filename) {
+            Ok(input_file) => {
                 let mut content = Vec::new();
                 let mut reader = BufReader::new(input_file);
                 reader.read_to_end(&mut content).unwrap();
 
-                let mut hasher = Sha1::new();
-                hasher.write_all(b"blob ").unwrap();
-                hasher
-                    .write_all(content.len().to_string().as_bytes())
-                    .unwrap();
-                hasher.write_all(b"\0").unwrap();
-                hasher.write_all(content.as_slice()).unwrap();
-                let sha = hasher.finalize();
-                let sha = format!("{:x}", sha);
+                let object = [
+                    b"blob ",
+                    content.len().to_string().as_bytes(),
+                    b"\0",
+                    content.as_slice(),
+                ]
+                .concat();
 
-                let dirname = &sha[0..2];
-                let filename = &sha[2..];
+                let mut hasher = Sha1::new();
+                hasher.write_all(&object)?;
+
+                let sha = hasher.finalize();
+                let object_hash = format!("{:x}", sha);
+
+                let dirname = &object_hash[0..2];
+                let filename = &object_hash[2..];
 
                 if write {
-                    DirBuilder::new()
-                        .create(format!(".git/objects/{dirname}"))
-                        .unwrap();
-
-                    let output_file =
-                        File::create(format!(".git/objects/{dirname}/{filename}")).unwrap();
-
+                    fs::create_dir(format!(".git/objects/{dirname}"))?;
+                    let output_file = File::create(format!(".git/objects/{dirname}/{filename}"))?;
                     let mut encoder = ZlibEncoder::new(output_file, Compression::best());
 
-                    encoder.write_all(b"blob ").unwrap();
-                    encoder
-                        .write_all(content.len().to_string().as_bytes())
-                        .unwrap();
-                    encoder.write_all(b"\0").unwrap();
-                    encoder.write_all(content.as_slice()).unwrap();
+                    encoder.write(&object)?;
                 }
 
-                println!("{}", sha);
-            } else {
-                return Err(std::io::Error::new(
-                    std::io::ErrorKind::NotFound,
-                    std::format!("No such file or directory: {}", &filename),
-                ));
+                println!("{}", object_hash);
             }
-        }
+            Err(_) => {
+                anyhow::bail!("No such file or directory: {}", &filename);
+            }
+        },
     }
 
     return Ok(());
